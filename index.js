f26edd99053fa4bb9bf152de3558937433afb24f
Cindy Lee <cindylee93@gmail.com>
2019-07-16 21:49:11 -0700

    resolved errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function formatQueryParams(params) {
-    const queryItems = Object.keys(params).map(key => `${[encodeURICompent(key)]}=${encodeURIComponent(params[keys])}`);
+    const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
     return queryItems.join('&');
 }
 
@@ -12,7 +12,7 @@ function displayResults(responseJson, maxResults) {
     $('.results-list').empty();
 
     for (let i=0; i < responseJson.data.length & i < maxResults; i++) {
-        $('.results-list').appened(`<li><h3><a href="${responseJson.data[i].url}">$${responseJson.data[i].fullName}</a></h3>
+        $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
         <p>${responseJson.data[i].description}</p></li>`);
     }
     $('.results').removeClass('hidden');
@@ -35,10 +35,10 @@ function getParks(baseUrl, stateArr, maxResults, apiKey) {
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => displayResults(responseJson, maxResults)
+        .then(responseJson => displayResults(responseJson, maxResults))
         .catch(err => {
             $('.js-error').text(`Something went wrong: ${err.message}`);
-        }));
+        });
 }
 
 function watchForm() {
